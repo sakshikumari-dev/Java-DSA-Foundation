ae0c4f959b88977bc0a3b6b1fbe8834c0b8b89b6
Sakshi Kumari <10asakshikumari@gmail.com>
2026-03-08 00:04:11 +0530

    bill calculator

--- InputDemo.java
+++ InputDemo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-//Enter cost of 3items from the user(using float datatype)-a pencil,a pen and an eraser. You have to output the total cost of the items back to the user as their bill.(Add on : try adding 18% gst tax to the items in the bill as an advanced problem.
+//Enter cost of 3items from the user(using float datatype)-a pencil,a pen and an eraser. You have to output the total cost of the items back to the user as their bill.(Add on : try adding 18% gst tax to the items in the bill as an advanced problem.git statsu
 
 
 
