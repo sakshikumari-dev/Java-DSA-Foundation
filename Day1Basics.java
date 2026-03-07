f1461461bee8b6445c41c1084b5d04aa1b58b290
Sakshi Kumari <10asakshikumari@gmail.com>
2026-03-07 23:52:18 +0530

    simple stationery bill generator with 18% GST

--- Day1Basics.java
+++ Day1Basics.java
@@ -1,4 +1,4 @@
-//My first start in coding world .
+//Day -0 entering the universe of code .
 public class Day1Basics{
     public static void main (String[]args){
     System.out.println("hello world!");
---
+++ InputDemo.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+//Enter cost of 3items from the user(using float datatype)-a pencil,a pen and an eraser. You have to output the total cost of the items back to the user as their bill.(Add on : try adding 18% gst tax to the items in the bill as an advanced problem.
+
+
+
+public class InputDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the price of a pencil :");
+        float pencilPrice= sc.nextFloat();
+        System.out.print("Enter the price of a pen :");
+        float penPrice= sc.nextFloat();
+        System.out.print("Enter the price of an eraser :");
+        float eraserPrice= sc.nextFloat();
+
+        float totalCost = (pencilPrice + penPrice + eraserPrice);
+        System.out.println("Bill of three items are: "+ totalCost);
+
+        // adding 18% gst to the bill
+
+        float newTotalCost = totalCost +(0.18f * totalCost );
+        System.out.println(newTotalCost);
+
+    }
+}
